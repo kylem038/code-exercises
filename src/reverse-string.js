77c0b4f26d0440f4a0827424455a04805de7f05e
kylem038 <misenckj@gmail.com>
2016-10-18 10:03:22 -0600

    reverse a string

---
+++ src/reverse-string.js
@@ -0,0 +1,10 @@
+module.exports = (reverseString) => {
+  return reverseString.split('').reverse().join('');
+};
+
+//Steve's example
+  // let result = '';
+// for(let i = string.length -1; i >= 0; i--) {
+  // result += string[i];
+// }
+  // return result;
